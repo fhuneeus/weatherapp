3e7653d7d78f11a1730433e1751246803a4b1abb
Federico Huneeus <fhuneeus@gmail.com>
2022-01-20 12:48:16 -0300

    Fixing https

--- src/Api.js
+++ src/Api.js
@@ -9,7 +9,7 @@ import moment from 'moment';
 export default async function getData(latitude, longitude) {
 
   let apiKey = "df2469f49522faeb2e0819ede3518475";
-  let url = "http://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&exclude=minutely&appid="+apiKey+"&units=metric";
+  let url = "https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&exclude=minutely&appid="+apiKey+"&units=metric";
 
   let response = await fetch(url);
   let processedResponse = await response.json();
